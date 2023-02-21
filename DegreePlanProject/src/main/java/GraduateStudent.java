22faf8a0a92a8d9394d2004310855c0b1decdcaa
rkg3913 <90530735+rkg3913@users.noreply.github.com>
2023-02-21 13:09:43 -0600

    Add files via upload

---
+++ DegreePlanProject/src/main/java/GraduateStudent.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class GraduateStudent{
+    HashMap<String, GraduateStudent> mapA = new HashMap<>();
+    
+    private String pdfContentArray[]; // Variable holds the entire content of the fileName specified PDF
+    GraduateStudent(String readInPDF){
+        setPdfContentArray(readInPDF.split("\\r?\\n"));
+    }
+
+    /**
+     * @return the pdfContentArray
+     */
+    public String[] getPDFContentArray() {
+        return pdfContentArray;
+    }
+
+    /**
+     * @param pdfContentArray the pdfContentArray to set
+     */
+    public void setPdfContentArray(String[] pdfContentArray) {
+        this.pdfContentArray = pdfContentArray;
+    }
+
+    
+    
+}
