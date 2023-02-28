7c0cae3939fe73afe477d472b254ef70ead04c3c
rkg3913 <90530735+rkg3913@users.noreply.github.com>
2023-02-28 15:09:43 -0600

    Update PDFReader.java

--- DegreePlanProject/src/main/java/PDFReader.java
+++ DegreePlanProject/src/main/java/PDFReader.java
@@ -26,29 +26,32 @@ public class PDFReader extends FileDropWindow{
     }
     
     /**
-     * @param filePath 
-     * This method is used to read-in the filePath specified PDF.  
+     * This method is used to read-in a PDF file specified by its file path.
+     *
+     * @param filePath the file path of the PDF file to be read
      */
-    public void readPDF(String filePath){
-        try (PDDocument document = PDDocument.load(new File(filePath))){
+    public void readPDF(String filePath) {
+        try ( PDDocument document = PDDocument.load(new File(filePath))) {
             // Extract the text from the PDF file
             PDFTextStripper textStripper = new PDFTextStripper();
             setReadInPDF(textStripper.getText(document));
-            
-        } catch (IOException e){
+
+        } catch (IOException e) {
+            // Handle the case where the specified file path is invalid or cannot be found
             System.out.println();
             System.out.println("An error has occurred! Most likely you have entered a file path to a file that doesn't exist or cannot be found.");
-            System.out.println("File path in question is: " + getFileDirectory());
-            
-            // Gives user another chance to enter a filename of a file that does exist and can be found. User is also given chance to end application.
-//            setFilePath(this.getFilePath());
-//            readPDF(this.getFilePath());   
-        } catch (NullPointerException e){
+            System.out.println("File path in question is: " + filePath);
+
+            // Gives user another chance to enter a file path of a file that does exist and can be found. User is also given chance to end application.
+            // setFilePath(this.getFilePath());
+            // readPDF(this.getFilePath());   
+        } catch (NullPointerException e) {
+            // Handle the case where the file path is null
             System.out.println();
-            System.out.println("An error has occurred! Filename is null.");
+            System.out.println("An error has occurred! File path is null.");
             System.out.println("Application has terminated due to error. Goodbye!");
             System.exit(0);
-        } 
+        }
     }
 }
 
